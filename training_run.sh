820d64f39200dddf67c650219fb03efd6dd70d9e
Philip Richardson <philip@morriscs.com>
2025-11-15 15:48:20 -0500

    Refactor ActorModel to implement double Q-learning; adjust training script for consistent discount factors and training intervals.

--- training_run.sh
+++ training_run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-python3 training.py --episodes 32 --discount-factor 0.0 --sample-size 15
+# python3 training.py --episodes 32 --discount-factor 0.0 --sample-size 15
 
-# python3 live_training.py --episodes 32 --discount-factor 0.1 --train-every 4
-python3 live_training.py --episodes 32 --discount-factor 0.5 --train-every 4
-python3 live_training.py --episodes 32 --discount-factor 0.9 --train-every 4
-# python3 live_training.py --episodes 32 --discount-factor 0.95 --train-every 4
+# python3 live_training.py --episodes 32 --discount-factor 0.1 --train-every 2
+# python3 live_training.py --episodes 32 --discount-factor 0.5 --train-every 2
+# python3 live_training.py --episodes 32 --discount-factor 0.9 --train-every 2
+# python3 live_training.py --episodes 32 --discount-factor 0.95 --train-every 2
 
 while true; do
-    python3 live_training.py --episodes 32 --discount-factor 0.99 --train-every 4
+    python3 live_training.py --episodes 32 --discount-factor 0.99 --train-every 2
 done
